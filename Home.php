e76465d362248415afd33f00dbec014545051d7f
MAWI17 <51213490+MAWI17@users.noreply.github.com>
2019-06-14 13:38:03 +0300

    From html to php

---
+++ Home.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MASWI MR
+ * Date: 26-May-19
+ * Time: 19:57
+ */
+session_start();
+include 'config.php';
+
+if (!isset($_SESSION['login_user'])){
+    header("location:Login.html");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home-BizMaster</title>
+    <link rel="stylesheet" type="text/css" href="BizMaster.css">
+    <script src="UserHome.js" language="JavaScript"></script>
+</head>
+<body>
+<div class="juu" onclick="coming()">
+    Intergrated Business Management System
+</div>                                                        <!--Part ya juu kabisa-->
+<span class="userdetails" id="userdetails"></span>
+<div class="kushoto" class="kushoto">
+    <a href="Mauzo.php">
+        <div class="vyandani">Mauzo</div>
+    </a>
+    <a href="Matumizi.php">
+        <div class="vyandani">
+        Matumizi
+        </div>
+    </a>
+    <div class="vyandani" onclick="coming()">
+        Msaada
+    </div>
+    <div class="nafasi"></div>
+    <a href="Logout.php">
+        <input type="submit" class="logout" value="Toka"/>
+    </a>
+</div>                                                          <!--Sidebar ya kushoto-->
+<div class="main" id="main">
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya matumizi ya leo yamefikia <span class="mainspans">Tshs 31,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+    <div class="mainpods">
+        Jumla ya mauzo ya leo yamefikia <span class="mainspans">Tshs 171,789/=</span>
+    </div>
+</div>                                                                    <!--main body part-->
+<div class="footer">
+    &copy;2019. All Rights Reserved.
+</div>
+</body>
+</html>
